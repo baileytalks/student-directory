4650fcee54ee8eff38f06015d607beca29e5895a
Bailey Kursar <bkursar@gmail.com>
2017-03-31 15:00:47 +0100

    Finished exercise 10, used the String method /.sub

---
+++ exercises/exercise10.rb
@@ -0,0 +1,150 @@
+## We've been using the chomp() method to get rid
+## of the last return character. Find another method
+## among those provided by the String class that could be
+## used for the same purpose (although it will require passing
+## some arguments).
+
+# asking for input
+
+@months = ['january', 'february', 'march',
+          'april', 'may', 'june', 'july',
+          'august', 'september', 'october',
+          'november', 'december']
+
+def input_students
+  puts "Please enter the student details"
+  puts "To finish, just type 'done'"
+
+  students = []
+
+  loop do
+    begin
+    puts "To start, type in their name"
+      name = gets.sub(/[\n]/, "")
+    end until name != ""
+      if name == "done"
+        break
+      end
+
+  puts "What is their hobby?"
+  hobby = gets.chomp
+  if hobby == ""
+    hobby = "nothing :("
+  elsif hobby == "done"
+    break
+  end
+
+  puts "What's their country of birth?"
+  country_of_birth = gets.chomp
+  if country_of_birth == ""
+    country_of_birth = "unknown :("
+  elsif
+    country_of_birth == "done"
+    break
+  end
+
+  puts "What's their height?"
+  height = gets.chomp
+  if height == ""
+    height = "unknown :("
+  elsif
+    height == "done"
+    break
+  end
+
+  puts "What's their favourite biscuit?"
+  biscuit = gets.chomp
+  if biscuit == ""
+    biscuit = "unknown, but probably custard creams"
+  elsif
+    biscuit == "done"
+    break
+  end
+
+  puts "What's their cohort?"
+  answer = gets.chomp.to_sym
+  if
+    @months.include?(answer.to_s) == true
+    cohort = answer
+  elsif
+    cohort == "done"
+    break
+  else
+    cohort = :april
+    puts "There's no #{answer} cohort, so we've assigned this student to April"
+  end
+
+  puts "We've assigned this student to #{cohort.capitalize}, is that OK?"
+  puts "Type y or n"
+  ok = gets.chomp
+    if ok == "n"
+      puts "What's their cohort?"
+      answer = gets.chomp.to_sym
+      if
+        @months.include?(answer.downcase.to_s) == true
+        cohort = answer
+      elsif
+        cohort == "done"
+        break
+      end
+    elsif
+      ok != 'y'
+      puts "That's neither 'y' or 'n', dummy!"
+    end
+
+    students << {name: name,
+                hobby: hobby,
+                country_of_birth: country_of_birth,
+                height: height,
+                biscuit: biscuit,
+                cohort: cohort}
+    if students.count == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+  # return the array of students
+end
+  students
+end
+
+def print_header
+  puts
+  puts "The students of Villains Academy".center(50, '--')
+  puts "------------".center(50, '--')
+end
+
+## Print only the names which are shorter than 12 characters
+def print(students)
+  students.each do |student|
+      puts "#{student[:name]}:
+            likes #{student[:hobby]},
+            is from #{student[:country_of_birth]},
+            is #{student[:height]}
+            and likes #{student[:biscuit]}s
+            (#{student[:cohort]} cohort)"
+      puts
+  end
+end
+
+def print_footer(students)
+  puts "------------".center(50, '--')
+  puts "Overall, we have #{students.count} great students!"
+  puts
+end
+
+students = input_students
+# nothing will happen until we call the methods
+print_header
+print(students)
+print_footer(students)
+
+## Grouping the cohorts
+students.group_by { |hash| hash[:cohort] }.each { |key, value|
+  puts "#{key.capitalize}"
+  puts "------------".center(20, '--')
+  value.each {|v|
+    puts v[:name]
+    }
+  puts
+  }
