7ef58d4b2c07e7bb4238b64710c2aeddd58be599
Bailey Kursar <bkursar@gmail.com>
2017-04-02 15:01:53 +0100

    Adding a save to file mehtod

--- step10.rb
+++ step10.rb
@@ -6,15 +6,26 @@
 
 @students = [] # an empty array accessible to all methods
 
+def save_students
+  file = File.open("students.csv","w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
 def show_students
   print_header
-  print_students_list
+  print_students_list(@students)
   print_footer(@students)
 end
 
@@ -24,6 +35,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else "I don't know what you meant"
@@ -61,13 +74,13 @@ def print_header
   puts "-------------"
 end
 
-def print_students_list(@students)
+def print_students_list(students)
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(@students)
+def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
