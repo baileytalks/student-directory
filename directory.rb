9412e2adebc10edc6559b8d0760889d9bf9c98ec
Bailey Kursar <bkursar@gmail.com>
2017-04-02 15:16:42 +0100

    Loading the list

--- directory.rb
+++ directory.rb
@@ -6,6 +6,15 @@
 
 @students = [] # an empty array accessible to all methods
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def save_students
   file = File.open("students.csv","w")
   @students.each do |student|
@@ -20,6 +29,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -37,6 +47,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else "I don't know what you meant"
